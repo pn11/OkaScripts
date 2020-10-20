b12214223e7b993985b759bd8867b8763529f731
oka <pn11@users.noreply.github.com>
2020-10-20 20:39:16 +0900

    fix

--- src/system/rewrite-bindaddress.sh
+++ src/system/rewrite-bindaddress.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 # Rewrite BindAddress in ~/.ssh/config by getting VPN address.
 address=$(ifconfig ppp0 | tail -1 | sed -E "s/.*inet\ (.*)\ -->.*/\1/g")
-sed -i -E "s/BindAddress (.*)/BindAddress ${address}/g" ~/.ssh/config
+sed -i.bak -E "s/BindAddress (.*)/BindAddress ${address}/g" ~/.ssh/config
