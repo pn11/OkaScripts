dc10d679e44c4d0c8655aaaf86d69413114a4cbb
oka@ideapad-duet <pn11@users.noreply.github.com>
2024-08-12 23:51:35 +0900

    Add concat-movie

---
+++ src/photo-et-movie/concat-movie.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# https://scrapbox.io/pn11/Concatenate_Movies_taken_by_Drive_Recorder_DRV-630
+
+set -euxo pipefail
+
+INPUTS=$*
+OUTNAME=concat-$1-etc.mp4
+
+for file in $(ls -1 $INPUTS)
+do
+    echo file \'$file\'
+done > inputs-ffmpeg.txt
+
+ffmpeg -f concat -safe 0 -i inputs-ffmpeg.txt -c:v copy -c:a aac $OUTNAME
+ffmpeg -i $OUTNAME -filter_complex "[0:v]setpts=0.1*PTS[v];[0:a]atempo=10[a]" -map "[v]" -map "[a]" ${OUTNAME}-10x.mp4
